cc77c7e45b420eb2f1b0adc6e7efbe724734fec2
valeriia.klimova <valeriia.klimova@x2sy.com>
2020-05-22 14:56:54 +0300

    Updates

---
+++ precache-manifest.6e18ebe2f4e120ff82917dbdd58cefa1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b956eb3aaafe1d41c7011ff33634f244",
+    "url": "/github/index.html"
+  },
+  {
+    "revision": "f461f9d44b082b53c7f1",
+    "url": "/github/static/css/main.f9fd2671.chunk.css"
+  },
+  {
+    "revision": "edb670250eafd99f2927",
+    "url": "/github/static/js/2.5f400208.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/github/static/js/2.5f400208.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f461f9d44b082b53c7f1",
+    "url": "/github/static/js/main.4b72c5ff.chunk.js"
+  },
+  {
+    "revision": "338dd00a4cb72f33f750",
+    "url": "/github/static/js/runtime-main.3d02493f.js"
+  }
+]);
